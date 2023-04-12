a25f768522c73a97a502016aa0ea9ef37df81315
pravinchalke <36146003+pravinchalke@users.noreply.github.com>
2023-04-12 18:08:35 +0530

    Add files via upload

---
+++ chocolateyinstall.ps1
@@ -0,0 +1,17 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = 'C:\Windows\System32\cscript.exe'
+
+$packageArgs = @{
+  packageName   = 'TestPkg1'
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' #only one of these: exe, msi, msu
+
+  softwareName  = 'TestPkg1*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  file			= $fileLocation
+  silentArgs    = "$toolsDir\Install.vbs /Install"
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyinstallpackage
+
---
+++ chocolateyuninstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$ErrorActionPreference = 'Stop' # stop on all errors
+$packageArgs = @{
+  packageName   = 'TestPkg1'
+  SoftwareName	= 'TestPkg1*'
+  file 		= 'C:\Windows\System32\cscript.exe'
+  fileType      = 'EXE' #only one of these: MSI or EXE (ignore MSU for now)
+  # MSI
+  silentArgs    = "$toolsDir\Install.vbs /Remove"
+  validExitCodes= @(0) # https://msdn.microsoft.com/en-us/library/aa376931(v=vs.85).aspx
+}
+
+
+    Uninstall-ChocolateyPackage @packageArgs
